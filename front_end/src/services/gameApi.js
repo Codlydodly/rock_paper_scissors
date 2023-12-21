db630918f7bbf2f4f916c987216ccf39c0922dbd
Zach Yewman <zach@Zachs-MacBook-Pro.local>
2023-12-21 23:05:38 +0000

    calls to the backend now work from the front end

--- front_end/src/services/gameApi.js
+++ front_end/src/services/gameApi.js
@@ -1,15 +1,14 @@
 
 export const fetchOpenGameIDs = async () => {
-    // try {
-        const path = `${process.env.REACT_APP_GAME_API_URL}/open_games`;
-        console.log('All Environment Variables:', process.env);
+    try {
         const response = await fetch(`${process.env.REACT_APP_GAME_API_URL}/open_games`);
-        const result = await response.text();
+        const result = await response.json();
+        console.log(result)
         return result;
-    // } catch (error) {
-    //     console.error('Error fetching data: ', error);
-    //     return [];
-    // }
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+        return [];
+    }
 };
 
 const playerOneTurn = async() => {
--- src/routes.py
+++ src/routes.py
@@ -4,9 +4,18 @@ from rock_paper_scissors.entities import move_from_str
 from rock_paper_scissors.interfaces.game_storage import GameStorage
 from fastapi import FastAPI
 from typing import List
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow all origins (replace '*' with specific origins if needed)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/player_1')
