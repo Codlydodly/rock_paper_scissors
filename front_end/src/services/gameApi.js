6b8cafde3abd681f4c7667d2eb73854f5a1500b9
Zach Yewman <zach@Zachs-MacBook-Pro.local>
2023-12-21 23:20:25 +0000

    removed unncessary console log

--- front_end/src/services/gameApi.js
+++ front_end/src/services/gameApi.js
@@ -3,7 +3,6 @@ export const fetchOpenGameIDs = async () => {
     try {
         const response = await fetch(`${process.env.REACT_APP_GAME_API_URL}/open_games`);
         const result = await response.json();
-        console.log(result)
         return result;
     } catch (error) {
         console.error('Error fetching data: ', error);
