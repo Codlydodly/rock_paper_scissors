616f8704c6e2748329f432a43d22e66a494ccbbd
Zach Yewman <zach@Zachs-MacBook-Pro.local>
2023-12-21 22:56:37 +0000

    now uses correct url for request

--- front_end/src/services/gameApi.js
+++ front_end/src/services/gameApi.js
@@ -1,10 +1,10 @@
 
 export const fetchOpenGameIDs = async () => {
     // try {
-        const response = await fetch('${process.env.REACT_APP_API_URL}/open_games');
-        const result = await response.json();
-        console.log('================HELLO ZACH=================');
-        console.log(result);
+        const path = `${process.env.REACT_APP_GAME_API_URL}/open_games`;
+        console.log('All Environment Variables:', process.env);
+        const response = await fetch(`${process.env.REACT_APP_GAME_API_URL}/open_games`);
+        const result = await response.text();
         return result;
     // } catch (error) {
     //     console.error('Error fetching data: ', error);
